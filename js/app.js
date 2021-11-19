bb7927b41e53d3980408ffae8b452648d615bf69
CarolinaTDH <74347883+CarolinaTDH@users.noreply.github.com>
2021-11-19 17:35:45 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var url= window.location.href;
-var swlocation= 'https://jesusz11-1.github.io/chatbreakingbad.github.io/sw.js';
+var swlocation= 'https://carolinatdh.github.io/chat.github.io//sw.js';
 
 if(navigator.serviceWorker){
     if(url.includes('localhost')){
